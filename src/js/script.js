c1a87fcba050735e9327ec3fb3698cc6738111ab
Denis <chehovskiyd@gmail.com>
2022-07-27 18:14:07 +0300

    add adaptiv first block

--- src/js/script.js
+++ src/js/script.js
@@ -9,3 +9,22 @@ hamburger.addEventListener("click", () => {
 close.addEventListener("click", () => {
   menu.classList.remove("active");
 });
+
+$(".modal__close").on("click", function () {
+  $(".modal__overlay, #thanks").fadeOut("slow");
+});
+
+$("form").submit(function (e) {
+  e.preventDefault();
+
+  $.ajax({
+    type: "POST",
+    url: "mailer/smart.php",
+    data: $(this).serialize(),
+  }).done(function () {
+    $(this).find("input").val("");
+    $(".modal__overlay, #thanks").fadeIn("slow");
+    $("form").trigger("reset");
+  });
+  return false;
+});
